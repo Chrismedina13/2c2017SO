6bb04ca71eae14f2f8568d413b7381118fedc87f
chrismedina13 <chrismedina13@hotmail.com>
2017-09-08 16:48:21 -0300

    Arreglos

--- YAMA/src/Headers/yama.h
+++ YAMA/src/Headers/yama.h
@@ -9,6 +9,7 @@
 #define HEADERS_WORKER_H_
 
 #include "configuracion.h"
+#include "logYAMA.h"
 #include <stdio.h>
 #include <stdlib.h>
 #define ARCHIVO_CONFIGURACION "/home/utnso/tp-2017-2c-s1st3m4s_0p3r4t1v0s/YAMA/yama.conf"
