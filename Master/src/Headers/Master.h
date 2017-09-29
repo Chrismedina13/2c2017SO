adb5f5bcab170ea025d30e13b458975535bc426f
chrismedina13 <chrismedina13@hotmail.com>
2017-09-29 16:21:12 -0300

    Protocolo

--- Master/src/Headers/Master.h
+++ Master/src/Headers/Master.h
@@ -14,6 +14,7 @@
 #include "logMaster.h"
 #include "pthread.h"
 #include <SO_lib/sockets.h>
+#include <SO_lib/Protocolo.h>
 
 #include "ComunicacionConYama.h"
 #define ARCHIVO_CONFIGURACION "/home/utnso/tp-2017-2c-s1st3m4s_0p3r4t1v0s/Master/master.conf"
---
+++ SO_lib/SO_lib/Protocolo.h
@@ -0,0 +1,25 @@
+/*
+ * Protocolo.h
+ *
+ *  Created on: 29/9/2017
+ *      Author: utnso
+ */
+
+#ifndef SO_LIB_PROTOCOLO_H_
+#define SO_LIB_PROTOCOLO_H_
+
+//YAMA - MASTER
+#define NOMBRE_ARCHIVO 1
+#define SOL_TRANSFORMACION 2
+#define FIN_TRANSFORMACION 3
+#define SOL_REDUCCION_LOCAL 4
+#define FIN_REDUCCION_LOCAL 5
+#define SOL_REDUCCION_GLOBAL 6
+#define FIN_REDUCCION_GLOBAL 7
+#define SOL_ALMACENADO_FINAL 8
+
+//MASTER - FS
+#define ALMACENADO_FINAL 9
+
+
+#endif /* SO_LIB_PROTOCOLO_H_ */
