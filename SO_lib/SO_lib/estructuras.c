6f74ba37c86bda45bc8eb300727b44883c8103e8
camila <camila.marino@live.com.ar>
2017-09-29 16:56:41 -0300

    Creando funciones para manejar la serializacion

--- SO_lib/SO_lib/estructuras.c
+++ SO_lib/SO_lib/estructuras.c
@@ -7,9 +7,12 @@
 
 #include "estructuras.h"
 
-RespuestaTransformacionYAMA* setRespuestaTransformacionYAMA(char* nodo,int puertoWorker,char* ipWorker,int bloque,int bytesOcupados,char* archivoTemporal){
+RespuestaTransformacionYAMA* setRespuestaTransformacionYAMA(char* nodo,
+		int puertoWorker, char* ipWorker, int bloque, int bytesOcupados,
+		char* archivoTemporal) {
 
-	RespuestaTransformacionYAMA* respuesta = malloc(sizeof(RespuestaTransformacionYAMA));
+	RespuestaTransformacionYAMA* respuesta = malloc(
+			sizeof(RespuestaTransformacionYAMA));
 	respuesta->archivoTemporal = archivoTemporal;
 	respuesta->nodo = nodo;
 	respuesta->puertoWorker = puertoWorker;
@@ -19,8 +22,64 @@ RespuestaTransformacionYAMA* setRespuestaTransformacionYAMA(char* nodo,int puert
 	return respuesta;
 }
 
-void destruirTransformacionYAMA(RespuestaTransformacionYAMA* respuesta){
+void destruirTransformacionYAMA(RespuestaTransformacionYAMA* respuesta) {
 
 	//Aca hace un free del malloc de la estructura
 }
 
+Paquete* createPaquete(uint32_t codigo, uint32_t tamanio, char* mensaje) {
+	Paquete *Paquete = calloc(1, sizeof(Paquete));
+	size_t sizeBuffer = (sizeof(char) * tamanio);
+	Paquete->mensaje = calloc(1, sizeBuffer);
+	memcpy(Paquete->mensaje, mensaje, sizeBuffer);
+	Paquete->tamanio = tamanio;
+	Paquete->codigo = codigo;
+	return Paquete;
+}
+void destruirPaquete(Paquete* package) {
+	if (package != NULL) {
+		if (package->mensaje != NULL)
+			free(package->mensaje);
+		free(package);
+	}
+}
+size_t sizePackage(Paquete *package) {
+	size_t total;
+	total = sizeof(uint32_t) + sizeof(uint32_t);
+	if (package->mensaje != NULL)
+		total += sizeof(char) * package->tamanio;
+	return total;
+}
+
+char* serializarPaquete(Paquete *package) {
+
+	char *serializarPaquete = calloc(1, sizePackage(package));
+
+	int offset = 0;
+	size_t size_to_send;
+
+	size_to_send = sizeof(uint32_t);
+	memcpy(serializarPaquete + offset, &(package->mensaje), size_to_send);
+	offset += size_to_send;
+
+	size_to_send = sizeof(uint32_t);
+	memcpy(serializarPaquete + offset, &(package->tamanio), size_to_send);
+	offset += size_to_send;
+
+	if (package->tamanio > 0) {
+		size_to_send = sizeof(char) * package->tamanio;
+		memcpy(serializarPaquete + offset, package->mensaje, size_to_send);
+	}
+
+	return serializarPaquete;
+}
+int enviarPaquete(int fileDescriptor, Paquete *package) {
+	int result;
+	char* serializedPkg = serializarPaquete(package);
+	result = send(fileDescriptor, serializedPkg, sizePackage(package), 0);
+	free(serializedPkg);
+	if (result == -1)
+		return -1; //SI DA ERROR
+
+	return 1; //SI ESTA OK
+}
--- SO_lib/SO_lib/estructuras.h
+++ SO_lib/SO_lib/estructuras.h
@@ -12,7 +12,7 @@
 
 //YAMA armara una lista con esta estructura , la serializara y mandara a Master que deserealizara
 
-typedef struct RespuestaTransformacionYAMA{
+typedef struct RespuestaTransformacionYAMA {
 	char* nodo;
 	int puertoWorker;
 	char* ipWorkwer;
@@ -20,7 +20,7 @@ typedef struct RespuestaTransformacionYAMA{
 	int bytesOcupados;
 	char* archivoTemporal;
 
-}RespuestaTransformacionYAMA;
+} RespuestaTransformacionYAMA;
 
 //paquete para mandar y recibir mensajes
 typedef struct Paquete {
@@ -29,4 +29,9 @@ typedef struct Paquete {
 	char* mensaje;
 } Paquete;
 
+//funciones
+void destruirPaquete(Paquete* package);
+size_t sizePackage(Paquete *package);
+char* serializarPaquete(Paquete *package);
+int enviarPaquete(int fileDescriptor, Paquete *package);
 #endif /* SO_LIB_ESTRUCTURAS_H_ */
