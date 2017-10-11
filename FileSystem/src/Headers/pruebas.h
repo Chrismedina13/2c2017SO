6e4467b272e32466ef5147f5be90601c9a24fd9b
ignacio <ignaciodelvalle2014@gmail.com>
2017-10-11 20:38:54 -0300

    prueba mmap

---
+++ FileSystem/src/Headers/pruebas.h
@@ -0,0 +1,14 @@
+/*
+ * pruebas.h
+ *
+ *  Created on: 11/10/2017
+ *      Author: utnso
+ */
+
+#include <unistd.h>
+#include <sys/mman.h>
+#include <stdio.h>
+#include <stdlib.h>
+#define tam_bloque 1024 //fijarse
+
+int pruebasmmap ();
