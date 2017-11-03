d7a15129100ac74859e6ed680479645207e066ac
giselle rodriguez <giselleariadna@gmail.com>
2017-11-03 17:17:11 -0300

    serializacion fs / yama

--- SO_lib/SO_lib/serializacion.c
+++ SO_lib/SO_lib/serializacion.c
@@ -43,3 +43,40 @@ return setbloque;
 }
 
 
+char* serializarUbicacionBloque( UbicacionBloque * ubicacionbloque){
+	char* ubicacionBloqueSerializado = malloc(sizeof(int)*2);
+	int desplazamiento =0 ;
+	serializarDato(ubicacionBloqueSerializado, &(ubicacionbloque->nodo), sizeof(int), &desplazamiento);
+	serializarDato(ubicacionBloqueSerializado,&(ubicacionbloque->bloqueDelNodoDeLaCopia), sizeof(int), &desplazamiento);
+	return ubicacionBloqueSerializado;
+}
+
+char * serializarUblicacionBloqueArchivo(UbicacionBloquesArchivo * estructura){
+char* estructuraSerializada = malloc(sizeof(int)*6);
+int desplazamiento=0;
+serializarDato(estructuraSerializada, &(estructura->parteDelArchivo), sizeof(int), &desplazamiento);
+serializarUbicacionBloque(estructuraSerializada, &(estructura->ubicacionCopia1), sizeof(int)*2, &desplazamiento);
+serializarUbicacionBloque(estructuraSerializada, &(estructura->ubicacionCopia2), sizeof(int)*2, &desplazamiento);
+serializarDato(estructuraSerializada, &(estructura->bytesOcupados),sizeof(int), &desplazamiento);
+		return estructuraSerializada;
+}
+
+
+UbicacionBloque deserializarUbicacionBloque(char* ubicacionbloqueserializado){
+	UbicacionBloque * ubicacionbloque = malloc(sizeof(int)*2);
+	int desplazamiento=0;
+	deserializarDato(ubicacionbloque->nodo, ubicacionbloqueserializado,sizeof(int), &desplazamiento);
+	deserializarDato(ubicacionbloque->bloqueDelNodoDeLaCopia, ubicacionbloqueserializado, sizeof(int), &desplazamiento);
+	return ubicacionbloque;
+}
+
+UbicacionBloquesArchivo deserializarUbicacionArchivo(char* UbicacionSerializada){
+UbicacionBloquesArchivo * ubicacionbloquesarchivos = malloc(sizeof(int)*6);
+int desplazamiento=0;
+deserializarDato(ubicacionbloquesarchivos->parteDelArchivo, UbicacionSerializada, sizeof(int), &desplazamiento);
+deserializarUbicacionBloque(ubicacionbloquesarchivos->ubicacionCopia1, UbicacionSerializada, sizeof(int)*2,&desplazamiento);
+deserializarUbicacionBloque(ubicacionbloquesarchivos->ubicacionCopia2, UbicacionSerializada, sizeof(int)*2, &desplazamiento);
+deserializarDato(ubicacionbloquesarchivos->bytesOcupados, UbicacionSerializada, sizeof(int), &desplazamiento);
+
+return ubicacionbloquesarchivos;
+}
