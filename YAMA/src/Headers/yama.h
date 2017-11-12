a514ecf2901f1e3d5771666b3e4ed5a4fd94796b
chrismedina13 <chrismedina13@hotmail.com>
2017-11-12 00:27:56 -0300

    Respuesta a Master hecha

--- YAMA/src/Headers/yama.h
+++ YAMA/src/Headers/yama.h
@@ -21,12 +21,7 @@
 #include "commons/bitarray.h"
 #include "SO_lib/FuncionesAuxiliares.h"
 #include "tabla_estados.h"
-typedef struct jobs{
-
-	int master; //FD del master que me mando el job
-	char* nombreDelArchivo; //nombre del archivo del job
-	int identificadorJob; //numero que se le asigno al job
-}Job;
+#include "SO_lib/estructuras.h"
 
 
 t_queue* jobsAPlanificar; // VARIABLE GLOBAL TIPO LISTA DONDE SE GUARDAN LOS JOBS A PLANIFICAR
@@ -34,6 +29,7 @@ sem_t semaforoYAMA; //Espera que llegue el job de MASTER para ejecutar la pate d
 t_list* listaDeWorkerTotales;
 t_list* listaDeJobs;
 t_list* ipYPuertoWoerkers;
+
 Job* crearJOB(int FDMAster,char* nombreDelJob);
 
 #define ARCHIVO_CONFIGURACION "/home/utnso/tp-2017-2c-s1st3m4s_0p3r4t1v0s/YAMA/yama.conf"
