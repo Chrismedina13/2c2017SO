ff4f44d4d008c8409ee82cabf04f2720e96ca86c
ignacio <ignaciodelvalle2014@gmail.com>
2017-11-23 18:50:00 -0300

    si rompe es culpa de camila

--- FileSystem/src/Headers/recuperacionFS.h
+++ FileSystem/src/Headers/recuperacionFS.h
@@ -5,8 +5,18 @@
  *      Author: utnso
  */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <unistd.h>
+#include <errno.h>
+
 #ifndef HEADERS_RECUPERACIONFS_H_
 #define HEADERS_RECUPERACIONFS_H_
+
+
 
 
 #endif /* HEADERS_RECUPERACIONFS_H_ */
