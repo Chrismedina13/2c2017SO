ce264669fa18ab395db661a13e6a87a23a580bbe
Ariel <arielvergara95@gmail.com>
2017-12-06 17:29:47 -0300

    crearEstructuras ok

--- SO_lib/SO_lib/estructuras.c
+++ SO_lib/SO_lib/estructuras.c
@@ -227,3 +227,34 @@ infoReduccionLocalParaWorker* crearInfoReduccionLocalParaWorker(t_list* listaArc
 
 	return info;
 }
+infoReduccionGlobalDeMasterParaWorker* crearInfoReduccionGlobalDeMasterParaWorker(t_list* listaArchivosReduccionLocal,char* archivoTemporalReduccionGlobal,script* scritpReduccionGlobal){
+	infoReduccionGlobalDeMasterParaWorker* info = malloc(tamanioListaDeArchivos(listaArchivosReduccionLocal)+strlen(archivoTemporalReduccionGlobal) + tamanioScript(scritpReduccionGlobal));
+	info->archivoTemporalReduccionGlobal=archivoTemporalReduccionGlobal;
+	info->listaArchivosReduccionLocal = listaArchivosReduccionLocal;
+	info->scriptReduccionGlobal = scritpReduccionGlobal;
+
+	return info;
+}
+
+infoParaReduccionGlobal* crearInfoParaReduccionGlobal(char* ipWorker,int puerto,char* archivoTemporalReduccionLocal){
+	infoParaReduccionGlobal* info = malloc(strlen(ipWorker)+ sizeof(int) + strlen(archivoTemporalReduccionLocal));
+	info->archivoTemporalReduccionLocal = archivoTemporalReduccionLocal;
+	info->ipWorker = ipWorker;
+	info->puerto = puerto;
+
+	return info;
+}
+
+infoReduccionGlobalDeWorkerParaWorker* crearInfoReduccionGlobalDeWorkerParaWorker(char* archivoTemporalLocalRequerido){
+	infoReduccionGlobalDeWorkerParaWorker* info = malloc(strlen(archivoTemporalLocalRequerido));
+	info->archivoTemporalLocalRequerido = archivoTemporalLocalRequerido;
+
+	return info;
+}
+
+almacenadoFinal* crearAlmacenadoFinal(char* archivoTemporalReduccionGlobal){
+	almacenadoFinal* archivo = malloc(strlen(archivoTemporalReduccionGlobal));
+	archivo->archivoTemporalReduccionGlobal = archivoTemporalReduccionGlobal;
+
+	return archivo;
+}
